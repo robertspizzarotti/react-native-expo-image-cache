a10501675b48829f9b94a31a2e58c0b02edd9772
Jezza <jgrevzie@gmail.com>
2019-09-14 21:06:45 +1000

    import {BlurView} from "expo-blur";

--- src/Image.js
+++ src/Image.js
@@ -2,7 +2,7 @@
 import * as _ from "lodash";
 import * as React from "react";
 import {Image as RNImage, Animated, StyleSheet, View, Platform} from "react-native";
-import {BlurView} from "expo";
+import {BlurView} from "expo-blur";
 import { type ____ImageStyleProp_Internal as ImageStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 import type {ImageSourcePropType} from "react-native/Libraries/Image/ImageSourcePropType";
 
